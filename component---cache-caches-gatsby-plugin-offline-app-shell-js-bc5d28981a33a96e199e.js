2180b6f5bc80bdbc8fe62492d7e6ce91a3c9c238
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-05-03 17:17:15 +0800

    first commit

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-bc5d28981a33a96e199e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_ghost=self.webpackChunkgatsby_starter_ghost||[]).push([[306],{1930:function(t,e,n){var u=n(5318);e.__esModule=!0,e.default=void 0;var r=u(n(5354)),a=u(n(7294)),s=function(t){function e(){return t.apply(this,arguments)||this}return(0,r.default)(e,t),e.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},e}(a.default.Component),l=s;e.default=l}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-bc5d28981a33a96e199e.js.map
